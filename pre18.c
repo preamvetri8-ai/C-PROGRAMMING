6c0c8e520880c9e925701fed8052c3d31a8010dd
preamvetri8-ai <preamvetri8@gmail.com>
2025-09-04 15:01:28 +0530

    Add files via upload

--- pre18.c
+++ pre18.c
@@ -2,11 +2,9 @@
 int main()
 {
 	int a,b,c;
-	printf("you date of brith in the year:");
+	printf("you date of brith in the year(dd mm yy):");
 	scanf("%d%d%d",&a,&b,&c);
-	printf("%d",a);
-	printf("%d",b);
-	printf("%d",c);
-	printf("date of birth is"%d,a//%d,b//%d,c);
+	printf("%d\n",a,b,c);
 	return 0;
+	
 }
