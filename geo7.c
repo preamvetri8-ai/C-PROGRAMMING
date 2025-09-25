2e86b8e967e303d201c0bc43cd88a653987be70a
preamvetri8-ai <preamvetri8@gmail.com>
2025-09-25 14:55:01 +0530

    Add files via upload

---
+++ geo7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	   scanf("%d %d %d",&a,&b,&c) ;
+    if (a*a+b*b==c*c||a+c*c==b*b||b*b+c*c==a*a){
+    	printf("given triangle is right angle triangle");
+	} else
+	   printf("is not a right angle triangle");
+	      return 0;
+}
